8fb946197a60c7510b94c435f0fdff665398e3d8
Mahesh <maheshkarande1985@gmail.com>
2023-03-04 01:30:51 +0530

    ThemeDone

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/consts/theme_data.dart';
+import 'package:newsapp/provider/dark_theme_provider.dart';
+import 'package:newsapp/screens/home_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
+
+  void getCurrentAppThem() async {
+    await themeChangeProvider.darkThemePrefs.getTheme();
+  }
+
+  @override
+  void initState() {
+    getCurrentAppThem();
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) {
+            return themeChangeProvider;
+          },
+        )
+      ],
+      child: Consumer<DarkThemeProvider>(
+        builder: (context,themeProvider,child) {
+
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: Style.themeData(themeProvider.getDarkTheme, context),
+            home: HomeScreen(),
+          );
+        }
+      ),
+    );
+  }
+}
